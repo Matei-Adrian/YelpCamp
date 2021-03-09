d1b8819440e7c7ed29efb365e537a6d6ee839f42
Matei-Adrian <mm_ady99@yahoo.com>
2021-03-09 17:54:15 +0200

    update

---
+++ models/campground.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const campgroundSchema = new mongoose.Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+module.exports = Campground;
