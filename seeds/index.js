c3d6ac71268add13e78555e57111a2540c43bcc3
Matei-Adrian <mm_ady99@yahoo.com>
2021-03-16 16:06:09 +0200

    update the style

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomRo = Math.floor(Math.random() * 270);
+        const price = Math.floor(Math.random() * 30) + 20;
         const camp = new Campground({
             location: `${roCities[randomRo].city}, ${roCities[randomRo].country}`,
-            title: `${name(descriptors)} ${name(places)}`
+            title: `${name(descriptors)} ${name(places)}`,
+            image: 'https://source.unsplash.com/collection/1114848',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, repellat doloribus error sint natus ut, quasi qui officia iure corporis animi voluptatibus nisi nulla, amet voluptatem commodi! Nisi, corporis fugiat?',
+            price
         })
         await camp.save();
     }
