fcd57b47224fd7e472288ffe7d763a56c4b08731
Matei-Adrian <mm_ady99@yahoo.com>
2021-03-25 18:26:26 +0200

    validation update

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
+const campgroundSchema = require('./schemas');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -29,6 +31,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
+const validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400);
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
@@ -42,7 +55,7 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new.ejs');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
     const addCampground = new Campground(req.body.campground);
     await addCampground.save();
     res.redirect(`/campgrounds/${addCampground._id}`);
@@ -60,7 +73,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit.ejs', { campground });
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const body = req.body.campground;
     const updateCampground = await Campground.findByIdAndUpdate(id, body, { new: true });
