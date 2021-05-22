3fbb6580fd97defe254c2f1d43a9713522a376be
Matei-Adrian <70067141+Matei-Adrian@users.noreply.github.com>
2021-05-23 01:21:58 +0300

    Revert "Uploading and Deleteing Images"
    This reverts commit 80ed17c3654868c3353a1549d4241aff2f28417e.

--- models/campground.js
+++ models/campground.js
@@ -2,21 +2,9 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
-// https://res.cloudinary.com/yelp-camp2/image/upload/w_300/v1621719809/YelpCamp/tjf89tenjk7lr39whdwl.jpg
-
-
-const imageSchema = new Schema({
-    url: String,
-    filename: String
-});
-
-imageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200');
-});
-
-const campgroundSchema = new Schema({
+const campgroundSchema = new mongoose.Schema({
     title: String,
-    images: [imageSchema],
+    image: String,
     price: Number,
     description: String,
     location: String,
@@ -36,7 +24,7 @@ campgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
         await Review.deleteMany({ _id: { $in: doc.reviews } });
     }
-});
+})
 
 const Campground = mongoose.model('Campground', campgroundSchema);
 module.exports = Campground;
--- schemas.js
+++ schemas.js
@@ -3,11 +3,10 @@ const campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0).max(100),
-        // images: Joi.string().required(),
+        image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
-    }).required(),
-    deleteImages: Joi.array()
+    }).required()
 });
 
 const reviewSchema = Joi.object({
