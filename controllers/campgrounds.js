d73fa5f29b7e99d14094a55bbf38db60ab1a1a7a
Matei-Adrian <70067141+Matei-Adrian@users.noreply.github.com>
2021-05-23 01:31:53 +0300

    Revert "Revert "Uploading and Deleteing Images""
    This reverts commit 3fbb6580fd97defe254c2f1d43a9713522a376be.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -11,8 +12,10 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res) => {
     const addCampground = new Campground(req.body.campground);
+    addCampground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     addCampground.author = req.user._id;
     await addCampground.save();
+
     req.flash('success', 'Successfully made a new campgroun!');
     res.redirect(`/campgrounds/${addCampground._id}`);
 };
@@ -47,6 +50,15 @@ module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const body = req.body.campground;
     const updateCampground = await Campground.findByIdAndUpdate(id, body, { new: true });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    updateCampground.images.push(...imgs);
+    await updateCampground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await updateCampground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } }, { new: true });
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${updateCampground._id}`);
 };
